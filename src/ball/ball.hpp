a85e31613df7f81c9bc4301d592e18d4b2931882
Sovereign Shahid <s.shahid1@share.epsb.ca>
2024-03-19 09:00:54 -0600

    refactored ball class

--- src/ball/ball.hpp
+++ src/ball/ball.hpp
@@ -3,14 +3,16 @@
 
 class Ball {
   private:
-	raylib::Vector2 position;
 	int size;
 	Color color;
 	float time;
-	raylib::Vector2 velocity;
 
   public:
-	Ball(raylib::Vector2 pos,int s = 25, raylib::Vector2 vel = raylib::Vector2(0,0), Color c = RAYWHITE);
+	
+  raylib::Vector2 position;
+	raylib::Vector2 velocity;
+
+	Ball(raylib::Vector2 pos, int s, raylib::Vector2 vel = raylib::Vector2(0,0), Color c = RAYWHITE);
 	raylib::Vector2 getVector();
 	void Update(float dt, raylib::Vector2 a, int screenWidth, int screenHeight);
 	void Collide(Ball *b);
